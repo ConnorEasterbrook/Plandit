381438b1e38b6fcd8ba9de3320f53c19ca67a9ed
Connor <connoreaster@gmail.com>
2022-06-22 21:03:19 +0100

    No Blazor

--- Program.cs
+++ Program.cs
@@ -18,10 +18,12 @@ namespace Plandit
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args).ConfigureWebHostDefaults(webBuilder =>
-            {
-                webBuilder.UseElectron(args);
-                webBuilder.UseStartup<Startup>();
-            });
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseElectron(args);
+                    webBuilder.UseEnvironment("Development");
+                    webBuilder.UseStartup<Startup>();
+                });
     }
 }
--- Startup.cs
+++ Startup.cs
@@ -2,14 +2,13 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using ElectronNET.API;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using ElectronNET.API;
 
 namespace Plandit
 {
@@ -23,11 +22,9 @@ namespace Plandit
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
-            services.AddServerSideBlazor();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -40,7 +37,6 @@ namespace Plandit
             else
             {
                 app.UseExceptionHandler("/Error");
-
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
@@ -50,21 +46,27 @@ namespace Plandit
 
             app.UseRouting();
 
+            app.UseAuthorization();
+
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapBlazorHub();
-                endpoints.MapFallbackToPage("/_Host");
+                endpoints.MapRazorPages();
             });
 
             if (HybridSupport.IsElectronActive)
             {
-                ElectronBootstrap();
+                CreateWindow();
             }
         }
 
-        async void ElectronBootstrap()
+        async void CreateWindow()
         {
-            await Electron.WindowManager.CreateWindowAsync();
+            var window = await Electron.WindowManager.CreateWindowAsync();
+
+            window.OnClosed += () =>
+            {
+                Electron.App.Quit();
+            };
         }
     }
 }
