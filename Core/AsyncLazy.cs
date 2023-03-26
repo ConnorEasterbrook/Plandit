6f6da2342de3a69cea6b73c04044a89e12405cb4
Connor <connoreaster@gmail.com>
2023-03-26 15:18:45 +0100

    AsyncLazy

---
+++ Core/AsyncLazy.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Plandit.Core
+{
+    /// <summary>
+    /// Asynchronous lazy initialization class. Use for resource-intensive objects.
+    /// This will defer the execution of an asynchronous operation until it is needed.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class AsyncLazy<T>
+    {
+        // Lazy defers creation of the object until needed, and then catches it for future use.
+        readonly Lazy<Task<T>> instance; // 
+
+        /// <summary>
+        /// AsyncLazy constructor to delegate and run a new task asynchronously.
+        /// </summary>
+        /// <param name="factory"></param>
+        public AsyncLazy(Func<T> factory)
+        {
+            instance = new Lazy<Task<T>>(() => Task.Run(factory));
+        }
+
+        /// <summary>
+        /// Return TaskAwaiter to support the use of "await."
+        /// </summary>
+        /// <returns></returns>
+        public TaskAwaiter<T> GetAwaiter()
+        {
+            return instance.Value.GetAwaiter();
+        }
+
+        public void Start()
+        {
+            Task unused = instance.Value; // Invoke the instance value to start the asynchronous operation.
+        }
+    }
+}
--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,47 +1,75 @@
-﻿using System.Collections.ObjectModel;
+﻿using Microsoft.Data.Sqlite;
+using Plandit.Core.Data;
+using System.Collections.ObjectModel;
 
 namespace Plandit;
 
 public partial class MainPage : ContentPage
 {
-	private ObservableCollection<Task> _tasks = new ObservableCollection<Task>();
-    public ObservableCollection<Task> tasks
+	// Create a list of tasks
+	private ObservableCollection<TaskClass> _tasks = new ObservableCollection<TaskClass>();
+    public ObservableCollection<TaskClass> tasks
 	{
 		get { return _tasks; }
 	}
 
     public MainPage()
 	{
-		InitializeComponent();
-		BindingContext = this;
+		InitializeComponent(); // Initialize the XAML
+		BindingContext = this; // Bind the XAML to the C# code
 	}
 
+	/// <summary>
+	/// Change the light mode of the application
+	/// </summary>
+	/// <param name="sender"></param>
+	/// <param name="e"></param>
 	private void ChangeLightMode(object sender, EventArgs e)
 	{
         /*Application.Current.UserAppTheme = count % 2 == 0 ? AppTheme.Light : AppTheme.Dark;*/
     }
 
+	/// <summary>
+	/// Handle task addition
+	/// </summary>
+	/// <param name="sender"></param>
+	/// <param name="e"></param>
 	private void AddTaskOnClick(object sender, EventArgs e)
 	{
 		if (!string.IsNullOrEmpty(taskEntry.Text))
 		{
-            tasks.Add(new Task { TaskItem = taskEntry.Text });
-            taskEntry.Text = string.Empty;
+			// Add task to list
+			tasks.Add(task);
+			taskEntry.Text = string.Empty;
         }
 	}
 
+	/// <summary>
+	/// Handle task deletion
+	/// </summary>
+	/// <param name="sender"></param>
+	/// <param name="e"></param>
 	private void DeleteTaskOnClick(object sender, EventArgs e)
 	{
-        var task = (sender as Button).BindingContext as Task;
+        var task = (sender as Button).BindingContext as TaskClass;
         if(task != null)
         {
+			// Remove task from list
             tasks.Remove(task);
         }
     }
 }
 
-public class Task
+/// <summary>
+/// Class that stores Task Information
+/// </summary>
+public class TaskClass
 {
-	public string TaskItem { get; set; }
+	/*public int Id { get; set; }*/
+	public string TaskTitle { get; set; }
+	/*public bool IsCompleted { get; set; }
+	public DateTime CreatedAt { get; set; }
+	public DateTime CompletedAt { get; set; }
+	public DateTime DeadlineAt { get; set; }*/
 }
 
