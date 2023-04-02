5dc53a5f97b1a9840673d5f6e71ad135e7feeefb
Connor <connoreaster@gmail.com>
2023-04-02 20:28:20 +0100

    Procedural Popup

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -3,6 +3,8 @@ using Plandit.Pages;
 using SQLite;
 using Microsoft.Maui.Controls;
 using System.Collections.ObjectModel;
+using CommunityToolkit.Maui.Views;
+using System.Runtime.CompilerServices;
 
 namespace Plandit;
 
@@ -12,6 +14,20 @@ public partial class MainPage : ContentPage
     public ObservableCollection<ProjectPlanPage> ProjectPlans
     {
         get { return _projectPlans; }
+    }
+
+    private string _projectName;
+    public string ProjectName
+    {
+        get { return _projectName; }
+        set { _projectName = value; OnPropertyChanged(); }
+    }
+
+    private string _projectDescription;
+    public string ProjectDescription
+    {
+        get { return _projectDescription; }
+        set { _projectDescription = value; OnPropertyChanged(); }
     }
 
     public MainPage()
@@ -47,16 +63,64 @@ public partial class MainPage : ContentPage
         await Navigation.PushAsync(planPage);
     }
 
-    private async void AddProjectOnClick(object sender, EventArgs e)
+    private async void AddProjectButtonClicked(object sender, EventArgs e)
     {
-        /*if(!string.IsNullOrEmpty(taskEntry.Text))
+        // Create the popup object
+        Popup popup = new Popup();
+
+        // Create a StackLayout to hold the input fields
+        StackLayout stackLayout = new StackLayout();
+
+        // Create two Entry fields for title and description and add the Entry fields to the StackLayout
+        Entry titleEntry = new Entry { Placeholder = "Title" };
+        stackLayout.Children.Add(titleEntry);
+
+        Entry descriptionEntry = new Entry { Placeholder = "Description" };
+        stackLayout.Children.Add(descriptionEntry);
+
+        // Create a Button for submitting the input
+        Button submitButton = new Button { Text = "Submit" };
+
+        submitButton.Clicked += async (s, args) =>
+        {
+            ProjectName = titleEntry.Text;
+            ProjectDescription = descriptionEntry.Text;
+
+            AddProjectOnClick();
+            popup.Close();
+        };
+
+        popup.Content = new Border
+        {
+            BackgroundColor = Application.Current.UserAppTheme == AppTheme.Dark ? GetColour("ExowebLightBackground") : GetColour("ExowebDarkBackground"),
+            Padding = new Thickness(20),
+            Content = stackLayout
+        };
+
+        await this.ShowPopupAsync(popup);
+    }
+
+    private Color GetColour(string colourName)
+    {
+        Color returnColour = Color.FromArgb("ffffff");
+        if (Application.Current.Resources.TryGetValue(colourName, out var colour))
+        {
+            returnColour = (Color)colour;
+        }
+
+        return returnColour;
+    }
+
+    private async void AddProjectOnClick()
+    {
+        if(!string.IsNullOrEmpty(_projectName))
         {
             // Add task to list
-            await App.ProjectRepository.AddProject(taskEntry.Text);
-            taskEntry.Text = string.Empty;
+            await App.ProjectRepository.AddProject(_projectName);
+            _projectName = string.Empty;
 
             ShowProjects();
-        }*/
+        }
     }
 
     private async void DeleteProjectOnClick(object sender, EventArgs e)
